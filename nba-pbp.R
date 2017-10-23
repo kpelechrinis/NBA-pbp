ac0d78884075174144ac0fa9b2cbd973b6928f2f
kpelechrinis <kostas.pelechrinis@gmail.com>
2017-10-23 15:37:03 -0400

    Update nba-pbp.R

--- nba-pbp.R
+++ nba-pbp.R
@@ -1,7 +1,7 @@
 library(rjson)
 library(stringr)
 
-n.games = 40 # Here you can set the number of games you want to download. This will download the first 30 games of the season
+n.games = 40 # Here you can set the number of games you want to download. This will download the first 40 games of the season
 
 for (i in 1:n.games){
 	pbpURL = paste0("http://data.nba.com/data/v2015/json/mobile_teams/nba/2017/scores/pbp/002170",str_pad(as.character(i),4,pad="0"),"_full_pbp.json")
