4a3d7370ab300ce5a015cccc5b0c0704cf38699d
kpelechrinis <kostas.pelechrinis@gmail.com>
2017-10-24 06:59:15 -0400

    Update nba-pbp.R

--- nba-pbp.R
+++ nba-pbp.R
@@ -7,6 +7,9 @@ for (i in 1:n.games){
 	pbpURL = paste0("http://data.nba.com/data/v2015/json/mobile_teams/nba/2017/scores/pbp/002170",str_pad(as.character(i),4,pad="0"),"_full_pbp.json")
 	pbpData <- fromJSON(file = pbpURL, method="C")
 	pbpDataf <- data.frame(matrix(unlist(pbpData$g$pd[[1]]$pla), ncol=16, byrow = TRUE))
+	for (j in 2:length(pbpData$g$pd)){
+		pbpDataf <- rbind(pbpDataf,data.frame(matrix(unlist(pbpData$g$pd[[j]]$pla), ncol=16, byrow = TRUE)))
+	}
 	colnames(pbpDataf) <- c("evt","cl","de","locX","locY","opt1","opt2","mtype","etype","opid","tid","pid","hs","vs","epid","oftid")
 	write.csv(pbpDataf,file=paste0(i,".csv"))
-} 
+}
